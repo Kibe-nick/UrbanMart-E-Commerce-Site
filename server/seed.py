73e34b3f376f76d0a4d7fc365679adbb8b74c055
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-10-17 13:58:16 +0300

    Proper implementation of relationships during seeding.

--- server/seed.py
+++ server/seed.py
@@ -1,5 +1,11 @@
-from models import Product
+import random
+from random import choice as rc
+from faker import Faker
+from models import Product, User
 from config import db, app
+
+faker = Faker()
+
 
 def seed_products():
     # Clear products table
@@ -47,17 +53,55 @@ def seed_products():
 
     image_file_path = 'server/static/images/products/'
 
-    for product in products:
-        new_product = Product(
+    product_instances = [
+        Product(
             name=product['name'],
             price=product['price'],
             image_url=f'{image_file_path}{product["image_filename"]}',
             description=product['description'],
         )
-        
-        db.session.add(new_product)
-        db.session.commit()
+        for product in products
+    ]
+
+    db.session.add_all(product_instances)
+    db.session.commit()
+
+    return product_instances
+
+def seed_users(product_instances):
+    # Clear users table
+    User.query.delete()
+    db.session.commit()
+
+    # Seed users
+    users = []
+    roles = ['user','admin']
+    for _ in range(10):
+        # Create user instance
+        user = User(
+            username=faker.user_name(),
+            role=random.choice(roles),
+            bio=faker.text(max_nb_chars=50)
+        )
+
+        password=faker.password()
+        user.password_hash=password
+
+        num_products = random.randint(1, 3)
+        assigned_products = random.sample(product_instances, num_products)
+
+        for product in assigned_products:
+            user.products.append(product)
+
+        users.append(user)
+
+    db.session.add_all(users)
+    db.session.commit()
+
+    return 'Database seeded successfully'
 
 if __name__ == '__main__':
     with app.app_context():
-        seed_products()
+        product_instances = seed_products()
+        # seed users after seeding products
+        seed_users(product_instances)
