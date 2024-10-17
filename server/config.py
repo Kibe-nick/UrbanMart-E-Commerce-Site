ba504ccf47fe39bbaa6632ec8e3756f97bb3a070
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-10-17 08:07:48 +0300

    Enabled CORS globally.

--- server/config.py
+++ server/config.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
@@ -24,3 +25,6 @@ db.init_app(app)
 
 # Initialize Flask-Restful API
 api = Api(app)
+
+# Enable CORS globally
+CORS(app)
