a1257fb35151f2300ba31f33183ecb215ac6ebcb
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-10-18 12:02:33 +0300

    Created the Navbar with routes.

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,35 @@
+// components/Navbar.js
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <h1>UrbanMart</h1>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <div className="navbar-buttons">
+        <Link to="/login">
+          <button className="login-btn">Login</button>
+        </Link>
+        <Link to="/signup">
+          <button className="signup-btn">Sign Up</button>
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
