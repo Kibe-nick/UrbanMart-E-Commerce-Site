cdf7875a2dcb94622ec9539049dc16a6bc490fdc
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-10-19 12:24:41 +0300

    Added a new model and implemented association proxy.

--- server/models.py
+++ server/models.py
@@ -1,33 +1,18 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.hybrid import hybrid_property
-from config import db
+from sqlalchemy.ext.associationproxy import association_proxy
 
-# Association table for many-to-many relationship
-user_product = db.Table(
-    'user_products',
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-    db.Column('product_id', db.Integer, db.ForeignKey('products.id'))
+from config import db, metadata
+
+# Association table for many-to-many relationship between products and orders
+order_product = db.Table(
+    'order_products',
+    metadata,
+    db.Column('product_id', db.Integer, db.ForeignKey('products.id')),
+    db.Column('order_id', db.Integer, db.ForeignKey('orders.id'))
 )
 
-class Product(db.Model, SerializerMixin):
-    __tablename__ = 'products'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    image_url = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
-
-    # Relationship mapping product to users
-    users = db.relationship('User', secondary=user_product, back_populates='products')
-
-    # Serialization rules
-    serialize_rules = ('-users.products',)
-
-    def __repr__(self):
-        return f'<Product ID: {self.id}, Name: {self.name}, Price: {self.price}>'
-    
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
@@ -37,11 +22,14 @@ class User(db.Model, SerializerMixin):
     role = db.Column(db.String, nullable=False, default='user')
     bio = db.Column(db.String, nullable=False, default='A new user who is still trying to find their way around.')
 
+    # Relationship mapping user to orders
+    orders = db.relationship('Order', back_populates='user', cascade='all, delete-orphan')
+
     # Relationship mapping user to products
-    products = db.relationship('Product', secondary=user_product, back_populates='users')
+    products = association_proxy('orders', 'product', creator=lambda product_obj: Order(product=product_obj))
 
     # Serialization rules
-    serialize_rules = ('-products.users',)
+    serialize_rules = ('-orders.user',)
 
     # Password encryption
     @hybrid_property
@@ -60,3 +48,41 @@ class User(db.Model, SerializerMixin):
 
     def __repr__(self):
         return f'User ID: {self.id}, Username: {self.username}, Role: {self.role}>'
+
+
+class Product(db.Model, SerializerMixin):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    image_url = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+
+    # Relationship mapping product to orders
+    orders = db.relationship('Order', secondary=order_product,  back_populates='products')
+
+    # Relationship mapping product to users
+    users = association_proxy('orders', 'user', creator=lambda user_obj: Order(user=user_obj))
+
+    # Serialization rules
+    serialize_rules = ('-users.products',)
+
+    def __repr__(self):
+        return f'<Product ID: {self.id}, Name: {self.name}, Price: {self.price}>'
+    
+class Order(db.Model, SerializerMixin):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    # Relationship mapping order to user
+    user = db.relationship('User', back_populates='orders')
+
+    # Relationship mapping order to products
+    products = db.relationship('Product', secondary=order_product, back_populates='orders')
+
+    # Serialization rules
+    serialize_rules = ('-user.orders',)
+    
