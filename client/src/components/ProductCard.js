226582b24280931cbf967af1bd44931a981f50d1
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-10-19 12:29:13 +0300

    Added a shopping cart icon.

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -3,13 +3,23 @@ import './ProductCard.css'
 
 function ProductCard({product}) {
   return (
-    <div className='product-card'>
-        <img src={product.image_url} alt={product.name} className='product-image'/>
-        <h3 className='product-name'>{product.name}</h3>
-        <p className='product-description'>${product.description}</p>
-        <p className='product-price'>${product.price}</p>
+    <div className="product-card">
+      <img
+        src={product.image_url}
+        alt={product.name}
+        className="product-image"
+      />
+      <h3 className="product-name">{product.name}</h3>
+      <p className="product-description">${product.description}</p>
+      <p className="product-price">${product.price}</p>
+      <img
+        src="/add-to-cart.png"
+        alt="Add to cart"
+        className="add-to-cart"
+        title='Add to cart'
+      />
     </div>
-  )
+  );
 }
 
 export default ProductCard
